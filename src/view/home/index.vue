e5d02c7e4c9076a725811b838dd63446352ce48e
xiaoyuan9527 <loveinmind1314>
2021-11-06 16:02:43 +0800

    props

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -4,24 +4,44 @@
         <span>
             <button @click="$router.push('/login')">去login</button>
             <button @click="$router.push('/list')">去list</button>
+            <br>
+            <span>子组件信息{{info}}</span>
             <!-- 父子传值->父传子 -->
-            <button @click="!state">传值</button>
-            <title v-if="state" :name="name" :age="age" />
-        </span>
+            <div>
+                <button @click="changeEvt">传值</button>
+                <!-- 传值用:name="name" 传递，子组件利用props接收 -->
+                <!-- 父组件自定义一个事件，设置一个方法 -->
+                <model-title @childFn="parentFn" v-if="state" :name="name" :age="age" />
+            </div>
+            </span>
     </div>
 </template>
 <script>
-    import Title from './title.vue'
+    import modelTitle from './modelTitle.vue'
     export default {
-        mixins: {
-            Title
+        components: {
+            modelTitle 
         },
         data() {
             return {
+                // info是子组件传回来的，必须先定义
+                info: '',
                 state: false,
                 name: '小花',
                 age: '18'
             }
         },
+        methods: {
+            changeEvt() {
+                this.state = !this.state
+                if (this.state){
+                    this.age ++
+                }
+            },
+            // 方法会默认接收值，把该值赋予到info
+            parentFn(data) {
+                this.info = data
+            } 
+        }
     }
 </script>
---
+++ src/view/home/modelTitle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <span>名字:{{name}}，年龄{{age}}</span>
+        <input type="text" v-model="info">
+        <button @click="clickEvt">信息传递</button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                info: ''
+            }
+        },
+        props: {
+            name: {
+                // 设置类型
+                type: String,
+                // 是否必传
+                required: true,
+            },
+            age: {
+                type: Number,
+                required: false,
+                // 默认值
+                default: 1
+            }
+        },
+        methods: {
+            clickEvt() {
+                // console.log(this.info);
+                // 通过$emit传递，父组件自定义的事件，后面为值
+                this.$emit('childFn', this.info)
+            }
+        }
+    }
+</script>
