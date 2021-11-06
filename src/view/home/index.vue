8c15b01ae219756556006414ce882744d67a58b3
xiaoyuan9527 <loveinmind1314>
2021-11-06 15:26:51 +0800

    less

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -4,9 +4,24 @@
         <span>
             <button @click="$router.push('/login')">去login</button>
             <button @click="$router.push('/list')">去list</button>
-            <span>
-                111
-            </span>
+            <!-- 父子传值->父传子 -->
+            <button @click="!state">传值</button>
+            <title v-if="state" :name="name" :age="age" />
         </span>
     </div>
 </template>
+<script>
+    import Title from './title.vue'
+    export default {
+        mixins: {
+            Title
+        },
+        data() {
+            return {
+                state: false,
+                name: '小花',
+                age: '18'
+            }
+        },
+    }
+</script>
---
+++ src/view/home/title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <span>this child</span>
+        <span>名字：{{name}}，年龄{{age}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            name: {
+                // 设置类型
+                type: String,
+                // 是否必传
+                required: true,
+            },
+            age: {
+                type: Number,
+                required: false,
+                // 默认值
+                default: 1
+            }
+        }
+    }
+</script>
